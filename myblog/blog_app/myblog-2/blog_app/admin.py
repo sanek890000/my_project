e65d92ec69bbde0597b7ffa05f3d20a87e25477e
sanek890000 <166908022+sanek890000@users.noreply.github.com>
2024-09-21 17:59:27 +0300

    Delete myblog/blog_app/myblog-2 directory

--- myblog/blog_app/myblog-2/blog_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date', 'views')
-    prepopulated_fields = {'slug': ('title',)}
-
-# Register your models here.
