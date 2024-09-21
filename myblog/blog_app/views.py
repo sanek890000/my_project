d2def2d632af12f75d77523480e9df8100911560
sanek890000 <166908022+sanek890000@users.noreply.github.com>
2024-09-21 18:03:10 +0300

    Add files via upload

--- myblog/blog_app/views.py
+++ myblog/blog_app/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def main_view(request):
     return render(request, 'main.html')
-
-
 def blog_view(request):
     return render(request, 'blog.html')
 
-
 def post_detail_view(request, slug):
-    return render(request, 'python_blog/post_detail.html')
+    return render(request, 'post_detail.html')
 
 
 def about_view(request):
-    user_count = 100  # Пример переменной для страницы "О проекте"
+    user_count = 100
     return render(request, 'about.html', {'user_count': user_count})
+
+
+def blog(request):
+    posts = Post.objects.all()
+    return render(request, 'blog.html', {'posts': posts})
+
+
+def post_by_slug(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
+    post.views += 1
+    post.save()
+    return render(request, 'post_detail.html', {'post': post})
 
 # Create your views here.
